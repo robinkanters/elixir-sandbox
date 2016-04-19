79393991ae41e02bab0f6b3d07851e453cf40be9
Robin Kanters <robin@anddare.com>
2016-04-19 14:45:22 +0200

    Added positive palindrome test

--- test/palindrome_test.exs
+++ test/palindrome_test.exs
@@ -4,4 +4,8 @@ defmodule PalindromeTest do
   test "robin is not a palindrome" do
     assert Palindrome.is_palindrome?("robin") == false
   end
+
+  test "toohottohoot is a palindrome" do
+    assert Palindrome.is_palindrome?("toohottohoot")
+  end
 end
