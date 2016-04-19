95a4bbe910526319199b46eb20cb0531419db065
Robin Kanters <robin@anddare.com>
2016-04-19 15:02:24 +0200

    Got rid of a ++

--- lib/flatten.ex
+++ lib/flatten.ex
@@ -11,5 +11,5 @@ defmodule Flatten do
   """
   def flatten([]), do: []
   def flatten([h|t]) when is_list(h), do: h ++ flatten(t)
-  def flatten([h|t]), do: [h] ++ flatten(t)
+  def flatten([h|t]), do: [h | flatten(t)]
 end
