88ef0c5d90ee1296520c3db4abd01fc3ab854598
Robin Kanters <robin@anddare.com>
2016-04-20 15:33:16 +0200

    Rewrote sort and merge to use pattern matching for invocation

--- lib/mergesort.ex
+++ lib/mergesort.ex
@@ -1,22 +1,19 @@
 defmodule Mergesort do
+  def sort([]) do [] end
+  def sort([a]) do [a] end
   def sort(l) do
-    cond do
-      l == [] -> []
-      length(l) <= 1 -> l
-      true ->
-        middle = div(length(l), 2)
-        left = Enum.slice(l, 0, middle)
-        right = Enum.slice(l, middle, length(l) - length(left))
-        left = sort(left)
-        right = sort(right)
-        merge(left, right)
-    end
+      middle = div(length(l), 2)
+      left = Enum.slice(l, 0, middle)
+      right = Enum.slice(l, middle, length(l) - length(left))
+      left = sort(left)
+      right = sort(right)
+      merge(left, right)
   end
 
+  def merge([], right) do right end
+  def merge(left, []) do left end
   def merge(left, right) do
     cond do
-      left == [] -> right
-      right == [] -> left
       hd(left) <= hd(right) -> [hd(left)] ++ merge(tl(left), right)
       true -> [hd(right)] ++ merge(left, tl(right))
     end
